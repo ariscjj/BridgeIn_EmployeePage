59fe6c8c8f1f0dc854c73d838f10a10fcd507828
Aris <ariscjj@outlook.com>
2022-06-23 16:45:42 +0100

    Attempting to add photo upload functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 //import boostrap from node modules 
+import { useNavigate } from 'react-router-dom';
+
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css'
@@ -10,14 +12,17 @@ import './App.css';
 import { Employee } from './models/employee.js';
 
 import EmployeeService from './services/employee.service.js'; 
+import FileService from './services/file.service.js';
 
 //import components from components folder
-import EmployeeTable from './components/employees/EmployeeTable';
+import EmployeeTable from './components/employees/GeneralTable';
 
-import EmployeeInput from './components/employees/EmployeeInput'; 
+import EmployeeInput from './components/employees/GeneralInput'; 
 
 export default function App() {
   const [employees, setEmployees] = useState([]);
+  const navigate = useNavigate();
+
 
   useEffect(() => {
     if (!employees.length){
@@ -26,16 +31,21 @@ export default function App() {
   }, []); 
 
   async function onInitialLoad(){
+    console.log("LOADING");
     const employees = await EmployeeService.fetchEmployees(); 
     setEmployees(employees);
   }
 
   async function onEmployeeCreate(photo, name, country, role, email, phone, status) {
-    // add the employee to the employees state 
-    //create the employee 
+    try {
+      // upload the file
+      const downloadUrl = await FileService.uploadImage(photo, (progress) => {
+        console.log('Upload Progress: ', progress);
+      });
+    
     const employee = await EmployeeService.createEmployee(
       new Employee(
-        null,
+        downloadUrl,
         photo, 
         name,
         country,
@@ -45,7 +55,27 @@ export default function App() {
         status
     ));
     setEmployees([...employees, employee]); 
+
+    // navigate('/');
+    } catch (err) {
+      // setError(err.message);
+    }
+
   }
+    
+    // const employee = await EmployeeService.createEmployee(
+    //   new Employee(
+    //     null,
+    //     photo, 
+    //     name,
+    //     country,
+    //     role,
+    //     email,
+    //     phone,
+    //     status
+    // ));
+    // setEmployees([...employees, employee]); 
+
 
   return (
     <div className="container my-5">
@@ -53,7 +83,9 @@ export default function App() {
         <h1>Employee List</h1>
         <hr></hr>
         <p>Add an Employee</p>
-        <EmployeeInput onEmployeeCreate={onEmployeeCreate} />
+        <EmployeeInput 
+          onEmployeeCreate={onEmployeeCreate}
+        />
         <EmployeeTable 
           employees={employees} />
       </div>
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 
 import { initializeApp } from "firebase/app";
-
 import { getFirestore } from "firebase/firestore"; 
+import { getStorage } from "firebase/storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
 
@@ -36,8 +36,10 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app); 
+const storage = getStorage(app);
 
 export{
-  db
+  db,
+  storage
 }; 
 
