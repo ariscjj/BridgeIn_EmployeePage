c8dda24c61506a7725efb0d6964409487adcc8a8
Aris <ariscjj@outlook.com>
2022-06-24 14:49:25 +0100

    added app.js back

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+//import boostrap from node modules 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import './App.css'; 
+//import the Employee class from the models folder 
+import { Employee } from './models/employee.js';
+import EmployeeService from './services/employee.service.js'; 
+//import components from components folder
+import EmployeeTable from './components/employees/EmployeeTable';
+import EmployeeInput from './components/employees/EmployeeInput'; 
+export default function App() {
+  const [employees, setEmployees] = useState([]);
+  useEffect(() => {
+    if (!employees.length){
+      onInitialLoad(); 
+    }
+  }, []); 
+  async function onInitialLoad(){
+    const employees = await EmployeeService.fetchEmployees(); 
+    setEmployees(employees);
+  }
+
+  async function onEmployeeCreate(photo, name, birthday, address, city, postalCode, country, role, email, phone, status) {
+    // add the employee to the employees state 
+    //create the employee 
+    const employee = await EmployeeService.createEmployee(
+      new Employee(
+        null,
+        photo, 
+        name,
+        birthday,
+        address,
+        city,
+        postalCode,
+        country,
+        role,
+        email,
+        phone,
+        status
+    ));
+    employees.set(employee);
+    setEmployees([...employees, employee]); 
+  }
+  return (
+    <div className="container my-5">
+      <div className="card card-body text-center">
+        <h1>Employee List</h1>
+        <hr></hr>
+        <p>Add an Employee</p>
+        <EmployeeInput onEmployeeCreate={onEmployeeCreate} />
+        <EmployeeTable 
+          employees={employees} />
+      </div>
+    </div>
+  )
+}
+
